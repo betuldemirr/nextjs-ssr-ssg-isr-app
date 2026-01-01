93b740181515d4fa846788d185a6793616568095
Betül Demir <betuldemir.007@gmail.com>
2026-01-02 02:14:47 +0300

    cms config admin.ts fixes

--- apps/cms/config/admin.ts
+++ apps/cms/config/admin.ts
@@ -1,20 +1,30 @@
-export default ({ env }) => ({
+type EnvFn = {
+  (key: string, defaultValue?: string): string;
+  bool(key: string, defaultValue?: boolean): boolean;
+  int?(key: string, defaultValue?: number): number;
+};
+
+type AdminConfigArgs = {
+  env: EnvFn;
+};
+
+export default ({ env }: AdminConfigArgs) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env("ENCRYPTION_KEY"),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
